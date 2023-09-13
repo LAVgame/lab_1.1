42c82202652d3b2f728d1f3c2713774c475a8f3d
LAVgame <vipaleksei007@gmail.com>
2023-09-13 08:41:30 +0300

    2 commit

--- 1/Program.cs
+++ 1/Program.cs
@@ -2,3 +2,9 @@
 Console.WriteLine("Напиши своё имя прохожий");
 string e = Console.ReadLine();
 Console.WriteLine($"Привет {e}");
+Console.WriteLine("Будем дружить?");
+e = Console.ReadLine();
+if (e == "да" || e == "Да") 
+Console.WriteLine($"CУПЕР привет друг!!");
+if (e == "нет" || e == "нет")
+    Console.WriteLine($"Ну зато у тебя новый знакомый появился :)");
